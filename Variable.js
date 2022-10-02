86a2786e3df9be3e4e8e9a9218f74422fbb53a0b
Nimeshpalsingh <97496951+Nimeshpalsingh@users.noreply.github.com>
2022-10-02 10:35:48 +0530

    Update Variable.js

--- Variable.js
+++ Variable.js
@@ -1,4 +1,3 @@
-// export  const Website = 'https://property99acres.com/back/';
+export  const Website = 'https://property99acres.com/back/';
 //  export  const Website = 'http://localhost/property99/';
-export  const Website = 'https://nextproperty2.vercel.app/back/';
 
