6a333fa699491ef14af8845e2dfcbf90addf92f3
Nimeshpalsingh <97496951+Nimeshpalsingh@users.noreply.github.com>
2022-10-02 08:23:48 +0530

    updated

--- mynext/componet/Navbar.js
+++ mynext/componet/Navbar.js
@@ -7,11 +7,11 @@ const Navbar = () => {
     <div>
        
        <div className="text-white d-flex d-flex justify-content-around fixed-bottom bg-primary ">
-  <Link  href='/'  ><div className="order-1 p-2"><ImHome/>Home</div></Link>
-  <Link  href='/AddProperty'  ><div className="order-2 p-2"><FaBuilding/>Add Property</div></Link>
+  <Link  href='/'><div className="order-1 p-2"><ImHome/>Home</div></Link>
+  <Link  href='/AddProperty'><div className="order-2 p-2"><FaBuilding/>Add Property</div></Link>
 
-  <Link  href='/Account/MyAds' ><div className="order-3 p-2"><FaDashcube/>MyAds</div></Link>
-  <Link  href='/Account/Account'  ><div className="order-4 p-2"><FaUserAlt/>Account</div></Link>
+  <Link  href='/Account/MyAds'><div className="order-3 p-2"><FaDashcube/>MyAds</div></Link>
+  <Link  href='/Account/Account'><div className="order-4 p-2"><FaUserAlt/>Account</div></Link>
 
 
 
