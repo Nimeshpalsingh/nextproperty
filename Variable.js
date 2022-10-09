7f1c3d7a1be9f222b5c9fb37e2af579f1b12f44b
Nimeshpalsingh <97496951+Nimeshpalsingh@users.noreply.github.com>
2022-10-09 08:45:15 +0530

    Update Variable.js

--- Variable.js
+++ Variable.js
@@ -1,3 +1,3 @@
-// export  const Website = 'https://www.property99acres.com/back/';
- export  const Website = 'http://localhost/property99/api/';
+export  const Website = 'https://www.property99acres.com/back/';
+//  export  const Website = 'http://localhost/property99/api/';
 
